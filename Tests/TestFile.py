a50177f995df96fa9b9a1bbd4c352f6a8d26d442
Neel <neelchikhal10@gmail.com>
2020-12-20 23:58:12 +0530

    initial commit - testfile.py

--- Tests/TestFile.py
+++ Tests/TestFile.py
@@ -1,11 +1,38 @@
-i=3
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+import os
+from pathlib import Path
+
+# from Config.Config import ConfigClass
 
 
-base_string_start="return check_box_elements_list["
-base_string_end="].checked"
+
+GECKODRIVER_PATH="D:\Python Selenium\HerokuPracticeSelenium\Drivers\geckodriver.exe"
+
+
+web_driver=webdriver.Firefox(executable_path=GECKODRIVER_PATH)
+web_driver.get("https://the-internet.herokuapp.com/horizontal_slider")
+web_driver.maximize_window()
+web_driver.implicitly_wait(20)
 
 
 
-for current in range(0,i):
-    final_js_query=base_string_start + str(current) + base_string_end
-    print(final_js_query)
+slider_element_locator=(By.XPATH,"//input[@type='range']")
+
+slider_element=WebDriverWait(web_driver,10).until(EC.visibility_of_element_located(slider_element_locator))
+
+action=ActionChains(web_driver)
+
+action.move_to_element(slider_element)
+# action.click_and_hold(slider_element)
+action.click(slider_element)
+for count in range(0,5):
+    action.send_keys(Keys.ARROW_LEFT)
+action.perform()
